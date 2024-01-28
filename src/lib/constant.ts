5fa4a9c87fd3325719c5843fb28fbfe581e96316
Nero One <info@nero1.net>
2024-01-28 11:05:31 +0800

    fix: use random.cc0-lib.wtf

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -2,6 +2,6 @@ const SITE_URL =
   process.env.NODE_ENV === "development"
     ? "https://hollywood-cl-freelance-tasks.trycloudflare.com" ||
       "http://localhost:1333"
-    : process.env.NEXT_PUBLIC_VERCEL_URL;
+    : "https://random.cc0-lib.wtf";
 
 export { SITE_URL };
